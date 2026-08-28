65692f52e0a41fb8988be41811024ee801feb71c
B.N.T. JAYAWARDHANA <jayawardananavodlk@gmail.com>
2026-08-28 17:55:09 +0530

    initial commit

---
+++ Backend/expernse-tracker/src/test/java/org/example/expernsetracker/ExpernseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expernsetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpernseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
